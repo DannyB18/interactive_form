fe29d1d830fe643a81f09ba941456cfb888a6626
Danny Bravo <danny.bravo18@gmail.com>
2022-08-13 11:43:15 -0400

    update job role selector

--- js/script.js
+++ js/script.js
@@ -1,3 +1,13 @@
 const nameInputField = document.getElementById("name");
+const otherJobInputField = document.getElementById("other-job-role");
+const roleSelector = document.getElementById("title");
 
 nameInputField.focus();
+otherJobInputField.style.display = 'none';
+
+roleSelector.addEventListener("change", (e) => {
+    const option = e.target;
+    if (option.tagName === "SELECT" && option.value === "other") {
+        otherJobInputField.style.display = 'initial';
+    }
+});
