c9a9c1981ad00503e5ee272113819754eeadbbb9
Danny Bravo <danny.bravo18@gmail.com>
2022-08-16 21:05:14 -0400

    add activity cost calculator

--- js/script.js
+++ js/script.js
@@ -16,22 +16,35 @@ roleSelector.addEventListener("change", (e) => {
     }
 });
 
+
 //T-Shirt Info
 shirtColorSelector.disabled = true;
 
 shirtDesignSelector.addEventListener("change", (e) => {
-
     shirtColorSelector.disabled = false;
     const colorOptions = document.querySelectorAll('#shirt-colors option');
-
     // Loop through each option in the select element and compare the data-theme to the selected design theme.
     colorOptions.forEach(option => {
         const optionTheme = option.dataset.theme;
-        if ( optionTheme !== shirtDesignSelector.value) {
-            option.style.display = 'none';
-        } else if ( optionTheme === shirtDesignSelector.value) {
-            option.style.display = 'initial';
+        if ( optionTheme !== shirtDesignSelector.value ) {
+            option.hidden = true;
+        } else if ( optionTheme === shirtDesignSelector.value ) {
+            option.hidden = false;
         }
     })
-
 })
+
+
+// Activity Registration
+const activites = document.getElementById('activities');
+const totalCostDisplay = document.getElementById('activities-cost');
+let totalCost = 0;
+
+    //Checks selected activity price and adds amount to total
+activities.addEventListener('change', (e) => {
+    const activity = e.target;
+    const activityCost = +activity.dataset.cost;
+    activity.checked ? totalCost += activityCost : totalCost -= activityCost;
+    totalCostDisplay.innerText = `Total: $${totalCost}`;
+});
+
